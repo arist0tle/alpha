63557bce0a04739deab6cd657cc2ec136b368f44
tanghaiyang <geektcp@163.com>
2019-02-22 15:32:42 +0800

    util random

--- alpha-util/alpha-util-common/src/main/java/util/RandomUtil.java
+++ alpha-util/alpha-util-common/src/main/java/util/RandomUtil.java
@@ -1,8 +1,7 @@
 package util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
-
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -11,19 +10,44 @@ import java.util.Random;
 @Slf4j
 public class RandomUtil {
 
-    public static void main(String[] args) {
-        RandomUtil o = new RandomUtil();
-        log.info("random int : {}", o.generateInt(10));
 
-    }
-
-
-    public int generateInt(int max){
-        final Random random = new Random(System.currentTimeMillis());
+    public static int randomInt(int max){
+        final Random random = new Random();
 
         return  random.nextInt(max);
     }
 
 
+    public static String randomString(int length){
+        final Random random = new Random();
+        String raw = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456";
+        int rawLength = raw.length();
+        String ret = "";
+        for(int i=0;i<length;i++) {
+            ret += raw.charAt(random.nextInt(rawLength));
+        }
+        return ret;
+    }
+
+    public static int randomInt(int[] arr){
+        final Random random = new Random();
+        int length = arr.length;
+        int seq = random.nextInt(length);
+        return  arr[seq];
+    }
+
+    public static String randomString(String[] arr){
+        final Random random = new Random();
+        int length = arr.length;
+        int seq = random.nextInt(length);
+        return  arr[seq];
+    }
+
+    public static <T> T random(List<T> arr){
+        final Random random = new Random();
+        int length = arr.size();
+        int seq = random.nextInt(length);
+        return  arr.get(seq);
+    }
 
 }
---
+++ alpha-util/alpha-util-common/src/test/java/util/RandomUtilTest.java
@@ -0,0 +1,51 @@
+package util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by tanghaiyang on 2019/2/22.
+ */
+@Slf4j
+public class RandomUtilTest {
+    public static void main(String[] args) {
+        RandomUtil o = new RandomUtil();
+        log.info("random int: {}", RandomUtil.randomInt(10));
+        log.info("random int: {}", RandomUtil.randomInt(10));
+        log.info("random int: {}", RandomUtil.randomInt(10));
+        log.info("random int: {}", RandomUtil.randomInt(10));
+
+        log.info("random str: {}", RandomUtil.randomString(4));
+        log.info("random str: {}", RandomUtil.randomString(4));
+        log.info("random str: {}", RandomUtil.randomString(4));
+        log.info("random str: {}", RandomUtil.randomString(5));
+        log.info("random str: {}", RandomUtil.randomString(64));
+        log.info("random str: {}", RandomUtil.randomString(128));
+
+        int[] arr = { 5, 4, 11, 23};
+
+        log.info("random int: {}", RandomUtil.randomInt(arr));
+        log.info("random int: {}", RandomUtil.randomInt(arr));
+        log.info("random int: {}", RandomUtil.randomInt(arr));
+
+        String[] arr2 = {"aaa", "bbb","ccc", "ddd"};
+        log.info("random arr int: {}", RandomUtil.randomString(arr2));
+        log.info("random arr int: {}", RandomUtil.randomString(arr2));
+        log.info("random arr int: {}", RandomUtil.randomString(arr2));
+        log.info("random arr int: {}", RandomUtil.randomString(arr2));
+
+
+        List<String> list = new LinkedList<>();
+        list.add("ooo");
+        list.add("ppp");
+        list.add("qqq");
+        list.add("sss");
+        log.info("random list int: {}", RandomUtil.random(list));
+        log.info("random list int: {}", RandomUtil.random(list));
+        log.info("random list int: {}", RandomUtil.random(list));
+        log.info("random list int: {}", RandomUtil.random(list));
+    }
+
+}

65bd77ed35aa8bcc8d47d0476d6f037e4f573179
tanghaiyang <geektcp@163.com>
2019-03-20 19:21:01 +0800

    rest-assured

--- alpha-util/alpha-util-http/src/test/java/AssuredTest.java
+++ alpha-util/alpha-util-http/src/test/java/AssuredTest.java
@@ -14,7 +14,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
 
@@ -24,10 +23,8 @@ import static io.restassured.RestAssured.given;
 @Slf4j
 public class AssuredTest {
 
-    public static void main(String[] args) {
+    private static final String url = "http://192.168.1.101:9000/query/work_graph/graphKExpand?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
 
-
-    }
 
 
     @Test
@@ -143,7 +140,6 @@ public class AssuredTest {
     @Test
     public void test5() throws Exception{
         log.info("test5=============");
-        String url = "http://192.168.1.101:9000/query/work_graph/graphKExpand?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
         String ret = HttpUtil.doGet(url);
         log.info("ret: {}", ret);
     }
@@ -152,7 +148,6 @@ public class AssuredTest {
     @Test
     public void test6() throws Exception{
         log.info("test6=============");
-        String url = "http://192.168.1.101:9000/query/work_graph/graphKExpand?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
         given().headers("Content-Type", "application/json")
                // .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
                 .header("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)")
@@ -166,7 +161,6 @@ public class AssuredTest {
     public void test7() throws Exception{
         log.info("test7=============");
         RestTemplate restTemplate = new RestTemplate();
-        String url = "http://192.168.1.101:9000/query/work_graph/graphKExpand?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
         URIBuilder builder = new URIBuilder(url);
         JSONObject ret = restTemplate.getForObject(builder.build(), JSONObject.class);
 
@@ -187,11 +181,54 @@ public class AssuredTest {
 //        params.put("gdbQuery", URLEncoder.encode(gdbQuery, "UTF-8"));
         params.put("gdbQuery", gdbQuery);
         for (Map.Entry<String, Object> entry : params.entrySet()) {
-            builder.addParameter(entry.getKey(), Objects.toString(entry.getValue(), ""));
+//            builder.addParameter(entry.getKey(), Objects.toString(entry.getValue(), ""));
+            builder.addParameter(entry.getKey(), URLEncoder.encode(gdbQuery, "UTF-8"));
         }
         JSONObject ret = restTemplate.getForObject(builder.build(), JSONObject.class);
 
         log.info(JSON.toJSONString(ret, SerializerFeature.PrettyFormat));
+    }
+
+//    @Test
+//    public void test9(){
+//        Map<String, Object> params;
+//        if (requestQo instanceof Map) {
+//            params = new HashMap<>((Map) requestQo);
+//        } else {
+//            params = JSONUtils.toMap(requestQo);
+//        }
+//        return params;
+//        Map<String, Object> params = this.getParameters(request);
+//        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url);
+//        for (Map.Entry<String, Object> entry : params.entrySet()) {
+//            builder.queryParam(entry.getKey(), Objects.toString(entry.getValue(), ""));
+//        }
+//        return restTemplate.getForObject(builder.build(true).toUri(), responseType);
+//
+//
+//    }
+
+    @Test
+    public void test10()throws Exception{
+        log.info("test10=============");
+        String url = "http://localhost:10030/api/url/test?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
+
+        String ret = HttpUtil.doGet(url);
+        log.info("ret: {}", ret);
+
+    }
+
+    @Test
+    public void test11()throws Exception{
+        log.info("test11=============");
+        String url = "http://localhost:10030/api/url/test?gdbQuery=%7B%22debug%22:true,%22direction%22:%22ANY%22,%22edgeTables%22:%5B%22all_to_skill%22,%22company_person%22%5D,%22endVertices%22:%5B%7B%22objectKey%22:%22p2%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22222222222222222222%22,%22schema%22:%22company%22%7D%5D,%22filterExpression%22:%22((t.type==%5C%22company%5C%22%20AND%20(t.city==%5C%22sayan%5C%22%20OR%20t.city==%5C%22dongguan%5C%22))%20OR%20(e.type==%5C%22all_to_skill%5C%22%20AND%20((e.quadrant%3E1%20AND%20e.quadrant%3C=800)%20OR%20(e.quadrant%3E=1000%20AND%20e.quadrant%3C=3000)))%20OR%20((t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%224%5C%22%20OR%20t.quadrant==%5C%225%5C%22))%20AND%20(t.type==%5C%22persons%5C%22%20AND%20(t.quadrant==%5C%2210%5C%22%20OR%20t.quadrant==%5C%2220%5C%22))))%22,%22graph%22:%22work_graph%22,%22internalOption%22:%7B%22debug%22:true%7D,%22maxDepth%22:10,%22maxSize%22:100,%22offset%22:44,%22size%22:100,%22startVertices%22:%5B%7B%22objectKey%22:%22c4%22,%22schema%22:%22company%22%7D,%7B%22objectKey%22:%22p3%22,%22schema%22:%22persons%22%7D,%7B%22objectKey%22:%22c1%22,%22schema%22:%22company%22%7D%5D,%22type%22:%22K_EXPAND%22,%22vertexTables%22:%5B%22persons%22,%22skill%22,%22company%22%5D%7D";
+
+        RestTemplate restTemplate = new RestTemplate();
+        URIBuilder builder = new URIBuilder(url);
+        String ret = restTemplate.getForObject(builder.build(), String.class);
+
+        log.info(ret);
+
     }
 
 

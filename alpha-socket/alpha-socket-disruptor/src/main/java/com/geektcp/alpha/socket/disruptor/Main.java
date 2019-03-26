c169c7d75bc27dde36e54cb1c338d5ee5e6d5b86
nagle <geektcp@163.com>
2019-03-26 22:26:40 +0800

    disruptor高并发套接字

---
+++ alpha-socket/alpha-socket-disruptor/src/main/java/com/geektcp/alpha/socket/disruptor/Main.java
@@ -0,0 +1,18 @@
+package com.geektcp.alpha.socket.disruptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * Created by HaiyangHome on 2019/3/26.
+ */
+@Slf4j
+public class Main {
+
+    @Test
+    public void test(){
+        log.info("test style");
+
+    }
+
+}

b20433b5c41dcefc1ffc6caa434ccf877b806d9a
tanghaiyang <geektcp@163.com>
2019-03-29 11:32:50 +0800

    springBoot kafka consume test case

--- alpha-hadoop/alpha-hadoop-kafka/src/test/java/com/geektcp/alpha/hadoop/kafka/consumer/AlphaPersistConsumerTest.java
+++ alpha-hadoop/alpha-hadoop-kafka/src/test/java/com/geektcp/alpha/hadoop/kafka/consumer/AlphaPersistConsumerTest.java
@@ -5,19 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
 
 /**
- * Created by chengmo on 2018/10/29.
+ * Created by tanghaiyang on 2018/10/29.
  */
 @Slf4j
-//@Component
-public class AlphaPersistConsumer {
+@Component
+public class AlphaPersistConsumerTest {
 
-//    @KafkaListener(id = "demo", topics = "deltaQ_GPE_1Q", group = "haizhi")
-    @KafkaListener(id = "demo2", topics = "mytest1", group = "haizhi")
+//    @KafkaListener(id = "demo1", topics = "deltaQ_GPE_1Q", group = "haizhi")
+    @KafkaListener(id = "demo", topics = "mytest1", group = "haizhi")
     public void listen(ConsumerRecord<Object, Object> record, Acknowledgment ack) {
-//        System.out.println("111111111");
-        log.info("records: {}", record.partition());
+        log.info("partition: {}", record.partition());
         test(record);
     }
 
@@ -25,5 +25,4 @@ public class AlphaPersistConsumer {
         log.info("topic: {} | value: {} | timestamp: {}", record.topic(), record.value(), record.timestamp());
 
     }
-
 }

f3c710525fb99109590f4c0039a932ba4f0efa07
tanghaiyang <geektcp@163.com>
2019-04-15 19:18:10 +0800

    arangodb data import

---
+++ alpha-db/alpha-db-arango/src/main/java/com/geektcp/alpha/db/arango/dao/ArangoDaoImpl.java
@@ -0,0 +1,12 @@
+package com.geektcp.alpha.db.arango.dao;
+
+/**
+ * Created by tanghaiyang on 2019/4/15.
+ */
+public class ArangoDaoImpl implements ArangoDao{
+
+    @Override
+    public void importDocument(String document) {
+
+    }
+}

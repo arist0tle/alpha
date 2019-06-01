5ccdd7d5e4601d8a3a46d071e94b16354d998300
tanghaiyang <tanghaiyang@stargraph.cn>
2019-06-01 18:25:21 +0800

    antlr example

--- alpha-util/alpha-util-antlr/gen/MathLexer.java
+++ alpha-util/alpha-util-antlr/gen/MathLexer.java
@@ -1,13 +1,8 @@
 // Generated from D:/alpha/alpha-util/alpha-util-antlr/src/main/resources\Math.g4 by ANTLR 4.7
 package com.zetyun.aiops.core.math;
-import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MathLexer extends Lexer {
--- alpha-util/alpha-util-antlr/gen/MathParser.java
+++ alpha-util/alpha-util-antlr/gen/MathParser.java
@@ -1,13 +1,8 @@
 // Generated from D:/alpha/alpha-util/alpha-util-antlr/src/main/resources\Math.g4 by ANTLR 4.7
 package com.zetyun.aiops.core.math;
-import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.misc.*;
-import org.antlr.v4.runtime.tree.*;
+
 import java.util.List;
-import java.util.Iterator;
-import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MathParser extends Parser {

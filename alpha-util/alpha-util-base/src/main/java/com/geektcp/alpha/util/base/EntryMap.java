c951c55a2ed2d32ca1b2f2f163aa5dbb6b883bfb
nagle <geektcp@163.com>
2019-08-14 12:47:23 +0800

    hash map code analysis

---
+++ alpha-util/alpha-util-base/src/main/java/com/geektcp/alpha/util/base/EntryMap.java
@@ -0,0 +1,33 @@
+package com.geektcp.alpha.util.base;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Created by TangHaiyang on 2019/8/14.
+ */
+public class EntryMap<K,V> implements Map.Entry<K,V>{
+    private int size;
+    private K key;
+    private V value;
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        this.value = value;
+        return value;
+    }
+}
--- alpha-util/alpha-util-base/src/test/java/ThyMapTest.java
+++ alpha-util/alpha-util-base/src/test/java/ThyMapTest.java
@@ -1,6 +1,9 @@
 import com.alibaba.fastjson.JSON;
+import com.geektcp.alpha.util.base.EntryMap;
 import com.geektcp.alpha.util.base.ThyMap;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 /**
  * Created by TangHaiyang on 2019/8/13.
@@ -8,11 +11,35 @@ import org.junit.Test;
 public class ThyMapTest {
 
     @Test
-    public void testHashMap(){
+    public void ThyMaptest(){
         ThyMap<String,Object> thyMap = new ThyMap<>();
-        thyMap.put("aaa", "ssss");
-        thyMap.put("ddddd", 11);
-
+        thyMap.put("Aa", "ssss1");
+        thyMap.put("BB", "ssss2");
+        thyMap.put("a2", 13);
+        thyMap.put("a3", 14);
+        thyMap.put("a4", 15);
+        thyMap.put("a5", 16);
+        thyMap.put("a6", 17);
+        thyMap.put("a7", 11);
+        thyMap.put("a8", 11);
+        thyMap.put("a9", 11);
         System.out.println(JSON.toJSONString(thyMap,true));
+
     }
+
+    @Test
+    public void HashcodeTest(){
+        System.out.println("Aa".hashCode());
+        System.out.println("BB".hashCode());
+    }
+
+
+    @Test
+    public void EntryMapTest(){
+        EntryMap<String, Object> entryMap = new EntryMap<>();
+
+
+
+    }
+
 }

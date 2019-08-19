23cf5c6399fb6840841cf028acdfc891945b09b3
nagle <geektcp@163.com>
2019-08-19 08:40:38 +0800

    WeakHashMap

--- alpha-util/alpha-util-base/src/test/java/ThyMapTest.java
+++ alpha-util/alpha-util-base/src/test/java/ThyMapTest.java
@@ -3,6 +3,11 @@ import com.geektcp.alpha.util.base.map.EntryMap;
 import com.geektcp.alpha.util.base.map.ThyMap;
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 /**
  * Created by TangHaiyang on 2019/8/13.
  */
@@ -31,7 +36,6 @@ public class ThyMapTest {
         System.out.println("BB".hashCode());
     }
 
-
     @Test
     public void EntryMapTest(){
         EntryMap<String, Object> entryMap = new EntryMap<>();
@@ -39,5 +43,33 @@ public class ThyMapTest {
 
 
     }
+
+    @Test
+    public void ConcurrentMapTest(){
+        ConcurrentMap<String,Object> map = new ConcurrentHashMap<>();
+        Object o = new Object();
+    }
+
+    /**
+     *  System.gc垃圾回收比较慢，所以有时候会打印1或者2，如果使用断点，几乎每次都是0
+     *  说明WeakHashMap里面的元素如果不用确实会被垃圾回收。
+     * @throws Exception
+     */
+    @Test
+    public void WeakHashMapTest()throws Exception{
+        Map<Object, Object> objectMap = new WeakHashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            String key = String.valueOf(i);
+            objectMap.put(key, new Object());
+            Thread.sleep(1000);
+            key = null;
+            System.gc();
+            System.out.println("Map size :" + objectMap.size());
+        }
+
+        System.gc();
+        System.out.println("Map size :" + objectMap.size());
+    }
+
 
 }

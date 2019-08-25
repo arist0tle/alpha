00a5fb33dfbcfc33e7f2817590617f6384eacd56
nagle <geektcp@163.com>
2019-08-25 22:19:29 +0800

    SynchronousQueue source code

--- alpha-util/alpha-util-queue/src/test/java/com/geektcp/alpha/util/queue/SynchronousQueueTest.java
+++ alpha-util/alpha-util-queue/src/test/java/com/geektcp/alpha/util/queue/SynchronousQueueTest.java
@@ -92,26 +92,5 @@ public class SynchronousQueueTest {
         System.out.println(element);
     }
 
-    @Test
-    public void sleep()throws Exception{
-        System.out.println(0);
-        Thread.sleep(0);
-        System.out.println(111111);
-        String lock = "lock";
 
-        ReentrantLock reentrantLock = new ReentrantLock();
-        Condition condition =  reentrantLock.newCondition();
-        reentrantLock.lock();
-        try{
-            reentrantLock.lock();
-            condition.await(0, TimeUnit.SECONDS);
-            System.out.println(33333);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        System.out.println(444444);
-        LockSupport.parkNanos(0);
-        System.out.println(555555);
-    }
 }

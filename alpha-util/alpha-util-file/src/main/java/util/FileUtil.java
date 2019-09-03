32bd734bb8846c6c48e957c146d5edf2d97dfa66
nagle <geektcp@163.com>
2019-09-04 00:22:35 +0800

    nio

--- alpha-util/alpha-util-file/src/main/java/util/FileUtil.java
+++ alpha-util/alpha-util-file/src/main/java/util/FileUtil.java
@@ -3,6 +3,8 @@ package util;
 import org.junit.Test;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Objects;
 
 /**
@@ -19,7 +21,7 @@ public class FileUtil {
             return;
         }
         File[] files = dir.listFiles();
-        if(Objects.isNull(files)){
+        if (Objects.isNull(files)) {
             return;
         }
         for (File file : files) {
@@ -27,6 +29,56 @@ public class FileUtil {
         }
     }
 
+    public static void copyFile(String src, String dist) throws IOException {
+        FileInputStream in = new FileInputStream(src);
+        FileOutputStream out = new FileOutputStream(dist);
 
+        byte[] buffer = new byte[20 * 1024];
+        int cnt;
+        /* read() 最多读取 buffer.length 个字节
+          返回的是实际读取的个数
+          返回 -1 的时候表示读到 eof，即文件尾
+        */
+        while ((cnt = in.read(buffer, 0, buffer.length)) != -1) {
+            out.write(buffer, 0, cnt);
+        }
+        in.close();
+        out.close();
+    }
 
+    public static void fastCopy(String src, String dist) throws IOException {
+        /* 获得源文件的输入字节流 */
+        FileInputStream fin = new FileInputStream(src);
+
+        /* 获取输入字节流的文件通道 */
+        FileChannel fcin = fin.getChannel();
+
+        /* 获取目标文件的输出字节流 */
+        FileOutputStream fout = new FileOutputStream(dist);
+
+        /* 获取输出字节流的文件通道 */
+        FileChannel fcout = fout.getChannel();
+
+        /* 为缓冲区分配 1024 个字节 */
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+
+        while (true) {
+            /* 从输入通道中读取数据到缓冲区中 */
+            int r = fcin.read(buffer);
+
+            /* read() 返回 -1 表示 EOF */
+            if (r == -1) {
+                break;
+            }
+
+            /* 切换读写 */
+            buffer.flip();
+
+            /* 把缓冲区的内容写入输出文件中 */
+            fcout.write(buffer);
+
+            /* 清空缓冲区 */
+            buffer.clear();
+        }
+    }
 }

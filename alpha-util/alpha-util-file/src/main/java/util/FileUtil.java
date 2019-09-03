aa77d32d3bd045a5d609c699f6615725657b784c
nagle <geektcp@163.com>
2019-09-03 22:52:46 +0800

    list files

--- alpha-util/alpha-util-file/src/main/java/util/FileUtil.java
+++ alpha-util/alpha-util-file/src/main/java/util/FileUtil.java
@@ -10,7 +10,7 @@ import java.util.Objects;
  */
 public class FileUtil {
 
-    private void listAllFiles(File dir) {
+    public static void listAllFiles(File dir) {
         if (dir == null || !dir.exists()) {
             return;
         }
@@ -27,55 +27,6 @@ public class FileUtil {
         }
     }
 
-    @Test
-    public void listAllFilesTest(){
-        File file = new File("F:\\tmp");
-        listAllFiles(file);
-    }
 
-    public static void main(String[] args) throws Exception {
-        FileUtil fileUtil = new FileUtil();
-        fileUtil.readFile();
-    }
-
-    @Test
-    public void readFile() throws Exception {
-        String resourceDir = "data/edge/part-000";
-        String path = this.getClass().getResource("/" + resourceDir).getPath();
-        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-        String line = "";
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
-        br.close();
-    }
-
-    @Test
-    public void readFileCharset() throws Exception{
-        String resourceDir = "data/edge/part-000";
-        String charset = "GBK";
-        String path = this.getClass().getResource("/" + resourceDir).getPath();
-        File file = new File(path);
-        FileInputStream fis = new FileInputStream(file);
-        BufferedReader br = new BufferedReader(new InputStreamReader(fis, charset));
-
-        String line = "";
-        while ((line = br.readLine()) != null) {
-            String[] arr = line.split("\\|@\\|");
-//            log.info("arr[4]: " + arr[4]);
-            break;
-        }
-    }
-
-    @Test
-    public void writeFile() throws Exception{
-        String resourceDir = "data/edge/part-000";
-        String path = this.getClass().getResource("/" + resourceDir).getPath();
-        BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path), true));
-        String line = "this is the content!";
-        bw.write(line);
-        bw.flush();
-        bw.close();
-    }
 
 }

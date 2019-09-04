4732ec8d41c3f88e67b92203108bddaa504ccc2f
nagle <geektcp@163.com>
2019-09-04 11:57:56 +0800

    oop

--- alpha-socket/alpha-socket-server/src/test/java/alpha/socket/thread/AppTest.java
+++ alpha-socket/alpha-socket-server/src/test/java/alpha/socket/thread/AppTest.java
@@ -19,25 +19,14 @@ import java.net.UnknownHostException;
 public class AppTest extends TestCase {
     private Socket socket;
 
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public AppTest(String testName) {
         super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite() {
         return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp() {
         assertTrue(true);
 
@@ -83,26 +72,21 @@ public class AppTest extends TestCase {
             out.flush();
 
             DataOutputBuffer buf2 = new DataOutputBuffer();
-            ConnectionBody wbody = new ConnectionBody("are you ok!!!");
-            wbody.write(buf2);
+            ConnectionBody writeBody = new ConnectionBody("are you ok!!!");
+            writeBody.write(buf2);
 
             bufLen = buf2.getLength();
             out.writeInt(bufLen);
             out.write(buf2.getData(), 0, bufLen);
             out.flush();
 
-            ConnectionBody rbody = new ConnectionBody();
-            rbody.readFields(in);
-
+            ConnectionBody readBody = new ConnectionBody();
+            readBody.readFields(in);
 			out.close();
 			in.close();
-
 			socket.close();
-
 			System.out.println("test over!!!");
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		}catch (IOException e) {
 			e.printStackTrace();
 		}
     }

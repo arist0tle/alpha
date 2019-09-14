adc03ed75adacdaacf0238892510e9a351ae8f81
nagle <geektcp@163.com>
2019-09-15 02:42:53 +0800

    红黑树构建

--- alpha-algorithm/alpha-algorithm-tree/src/main/java/com/geektcp/alpha/algorithm/tree/binary/RedBlackTree.java
+++ alpha-algorithm/alpha-algorithm-tree/src/main/java/com/geektcp/alpha/algorithm/tree/binary/RedBlackTree.java
@@ -1,21 +1,23 @@
 package com.geektcp.alpha.algorithm.tree.binary;
 
+import org.junit.Test;
+
 /**
  * Created by tanghaiyang on 2019/1/21.
  */
 public class RedBlackTree {
 
-    public static void main(String[] args) {
-
-    }
-
-    public class TreeNode {
-        private String value;
+    private static final class TreeNode {
+        private int value;
         private boolean red;
 
         private TreeNode parent;
         private TreeNode left;
         private TreeNode right;
+
+        TreeNode(int value){
+            this.value = value;
+        }
 
         public void leftRotate() {
             TreeNode rawLeft = left;
@@ -28,6 +30,27 @@ public class RedBlackTree {
             right = parent;
             right.left = rawRight;
         }
+
+        public void put(TreeNode treeNode){
+            // do something
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10);
+
+        TreeNode one = new TreeNode(20);
+        TreeNode two = new TreeNode(30);
+        TreeNode three = new TreeNode(40);
+
+        root.left = one;
+        root.right = two;
+
+
+
+
     }
 
 

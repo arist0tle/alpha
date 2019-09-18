fa4c7ce091131261c47761604026439a1e9339fa
nagle <geektcp@163.com>
2019-09-18 13:23:03 +0800

    完善文件操作工具类

--- alpha-common/alpha-common-base/src/main/java/alpha/common/base/util/FileUtils.java
+++ alpha-common/alpha-common-base/src/main/java/alpha/common/base/util/FileUtils.java
@@ -4,10 +4,9 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -136,5 +135,75 @@ public class FileUtils {
      */
     public static String getResourcePath() {
         return FileUtils.class.getResource("/").getPath();
+    }
+
+    public static void listAllFiles(File dir) {
+        if (dir == null || !dir.exists()) {
+            return;
+        }
+        if (dir.isFile()) {
+            System.out.println(dir.getName());
+            return;
+        }
+        File[] files = dir.listFiles();
+        if (Objects.isNull(files)) {
+            return;
+        }
+        for (File file : files) {
+            listAllFiles(file);
+        }
+    }
+
+    public static void copyFile(String src, String dist) throws IOException {
+        FileInputStream in = new FileInputStream(src);
+        FileOutputStream out = new FileOutputStream(dist);
+
+        byte[] buffer = new byte[20 * 1024];
+        int cnt;
+        /* read() 最多读取 buffer.length 个字节
+          返回的是实际读取的个数
+          返回 -1 的时候表示读到 eof，即文件尾
+        */
+        while ((cnt = in.read(buffer, 0, buffer.length)) != -1) {
+            out.write(buffer, 0, cnt);
+        }
+        in.close();
+        out.close();
+    }
+
+    public static void fastCopy(String src, String dist) throws IOException {
+        /* 获得源文件的输入字节流 */
+        FileInputStream fin = new FileInputStream(src);
+
+        /* 获取输入字节流的文件通道 */
+        FileChannel fcin = fin.getChannel();
+
+        /* 获取目标文件的输出字节流 */
+        FileOutputStream fout = new FileOutputStream(dist);
+
+        /* 获取输出字节流的文件通道 */
+        FileChannel fcout = fout.getChannel();
+
+        /* 为缓冲区分配 1024 个字节 */
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+
+        while (true) {
+            /* 从输入通道中读取数据到缓冲区中 */
+            int r = fcin.read(buffer);
+
+            /* read() 返回 -1 表示 EOF */
+            if (r == -1) {
+                break;
+            }
+
+            /* 切换读写 */
+            buffer.flip();
+
+            /* 把缓冲区的内容写入输出文件中 */
+            fcout.write(buffer);
+
+            /* 清空缓冲区 */
+            buffer.clear();
+        }
     }
 }

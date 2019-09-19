f22c542bd8cedff627e13df50d3aeff7e4f48e4e
nagle <geektcp@163.com>
2019-09-19 22:32:27 +0800

    shell脚本 开源组件

---
+++ alpha-shell/install_redis.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+wget http://download.redis.io/releases/redis-3.2.3.tar.gz
+
+groupadd redis
+useradd -g redis redis
+passwd redis
+
+mkdir -p /usr/local/redis-3.2.3/{bin,conf,run,log,db}
+cd /usr/local
+ln -s redis-3.2.3 redis
+chown -R redis.redis /usr/local/redis-3.2.3
+
+
+cd /usr/local/src
+tar xzf redis-3.2.3.tar.gz
+
+cd redis-3.2.3			
+make
+
+
+vi ~/.bash_profile
+source ~/.bash_profile
+
+cd /usr/local/src/redis-3.2.3
+cp redis.conf sentinel.conf  /usr/local/redis/conf
+cp src/{mkreleasehdr.sh,redis-benchmark,redis-check-aof,redis-check-rdb,redis-cli,redis-sentinel,redis-server,redis-trib.rb} /usr/local/redis/bin/
+
+
+cd /usr/local/redis/conf/
+cp redis.conf redis.conf_raw
+
+vim redis.conf
+
+
+cd /usr/local/redis
+bin/redis-server conf/redis.conf
+
+
+redis-cli
+
+
+
+
+
+

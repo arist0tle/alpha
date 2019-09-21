548b60b1d527fbfa3cfd2cbb1432c4151fed4045
nagle <geektcp@163.com>
2019-09-21 22:43:58 +0800

    design pattern: bridge

---
+++ alpha-design/alpha-design-pattern/src/main/java/com.geektcp.alpha.design.pattern/bridge/Sony.java
@@ -0,0 +1,21 @@
+package com.geektcp.alpha.design.pattern.bridge;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class Sony extends TV {
+    @Override
+    public void on() {
+        System.out.println("Sony.on()");
+    }
+
+    @Override
+    public void off() {
+        System.out.println("Sony.off()");
+    }
+
+    @Override
+    public void tuneChannel() {
+        System.out.println("Sony.tuneChannel()");
+    }
+}
---
+++ alpha-design/alpha-design-pattern/src/test/java/com/geektcp/alpha/design/pattern/bridge/RemoteControlTest.java
@@ -0,0 +1,18 @@
+package com.geektcp.alpha.design.pattern.bridge;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class RemoteControlTest {
+
+    public static void main(String[] args) {
+        RemoteControl remoteControl1 = new ConcreteRemoteControl1(new RCA());
+        remoteControl1.on();
+        remoteControl1.off();
+        remoteControl1.tuneChannel();
+        RemoteControl remoteControl2 = new ConcreteRemoteControl2(new Sony());
+        remoteControl2.on();
+        remoteControl2.off();
+        remoteControl2.tuneChannel();
+    }
+}

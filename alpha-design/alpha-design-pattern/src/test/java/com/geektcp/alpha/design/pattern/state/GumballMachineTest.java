bd2d8804bd96e69413476da1dac8557e508b51a2
nagle <geektcp@163.com>
2019-09-21 22:30:47 +0800

    design pattern: state

---
+++ alpha-design/alpha-design-pattern/src/test/java/com/geektcp/alpha/design/pattern/state/GumballMachineTest.java
@@ -0,0 +1,32 @@
+package com.geektcp.alpha.design.pattern.state;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class GumballMachineTest {
+
+    public static void main(String[] args) {
+        GumballMachine gumballMachine = new GumballMachine(5);
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.ejectQuarter();
+        gumballMachine.turnCrank();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.ejectQuarter();
+
+        gumballMachine.insertQuarter();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+        gumballMachine.insertQuarter();
+        gumballMachine.turnCrank();
+    }
+}

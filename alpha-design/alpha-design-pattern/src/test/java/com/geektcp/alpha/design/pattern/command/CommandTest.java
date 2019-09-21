a82c8cc548c231ac6f096553ae2e042a9fd7d550
nagle <geektcp@163.com>
2019-09-21 22:11:22 +0800

    design pattern: command

---
+++ alpha-design/alpha-design-pattern/src/test/java/com/geektcp/alpha/design/pattern/command/CommandTest.java
@@ -0,0 +1,17 @@
+package com.geektcp.alpha.design.pattern.command;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class CommandTest {
+    public static void main(String[] args) {
+        Invoker invoker = new Invoker();
+        Light light = new Light();
+        Command lightOnCommand = new LightOnCommand(light);
+        Command lightOffCommand = new LightOffCommand(light);
+        invoker.setOnCommand(lightOnCommand, 0);
+        invoker.setOffCommand(lightOffCommand, 0);
+        invoker.onButtonWasPushed(0);
+        invoker.offButtonWasPushed(0);
+    }
+}

e5e109ea44f04a327f408cccf4379254878a5f44
nagle <geektcp@163.com>
2019-09-21 22:14:16 +0800

    design pattern: interpreter

---
+++ alpha-design/alpha-design-pattern/src/main/java/com.geektcp.alpha.design.pattern/interpreter/Expression.java
@@ -0,0 +1,8 @@
+package com.geektcp.alpha.design.pattern.interpreter;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public abstract class Expression {
+    public abstract boolean interpret(String str);
+}

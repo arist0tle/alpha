a37e2833d582e85fc0861a0ec684ed33cc61a8b8
nagle <geektcp@163.com>
2019-09-21 21:56:40 +0800

    design pattern: factory method

---
+++ alpha-design/alpha-design-pattern/src/main/java/com.geektcp.alpha.design.pattern/factorymethod/ConcreteFactory2.java
@@ -0,0 +1,13 @@
+package com.geektcp.alpha.design.pattern.factorymethod;
+
+import com.geektcp.alpha.design.pattern.simplefactory.ConcreteProduct;
+import com.geektcp.alpha.design.pattern.simplefactory.Product;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class ConcreteFactory2 extends Factory {
+    public Product factoryMethod() {
+        return new ConcreteProduct();
+    }
+}

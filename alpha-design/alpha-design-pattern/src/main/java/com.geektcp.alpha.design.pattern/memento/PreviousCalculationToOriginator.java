a611aeda6629659d6504db8eab7fead0765ae5ef
nagle <geektcp@163.com>
2019-09-21 22:22:54 +0800

    design pattern: memento

---
+++ alpha-design/alpha-design-pattern/src/main/java/com.geektcp.alpha.design.pattern/memento/PreviousCalculationToOriginator.java
@@ -0,0 +1,9 @@
+package com.geektcp.alpha.design.pattern.memento;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public interface PreviousCalculationToOriginator {
+    int getFirstNumber();
+    int getSecondNumber();
+}

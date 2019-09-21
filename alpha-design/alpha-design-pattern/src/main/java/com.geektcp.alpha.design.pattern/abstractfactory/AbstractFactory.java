bf2dc3d4cf9e54b8830ea9d8c9d33cf8d954098a
nagle <geektcp@163.com>
2019-09-21 22:00:47 +0800

    design pattern: abstractfactory

---
+++ alpha-design/alpha-design-pattern/src/main/java/com.geektcp.alpha.design.pattern/abstractfactory/AbstractFactory.java
@@ -0,0 +1,9 @@
+package com.geektcp.alpha.design.pattern.abstractfactory;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public abstract class AbstractFactory {
+    abstract AbstractProductA createProductA();
+    abstract AbstractProductB createProductB();
+}

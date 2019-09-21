998a6df83fe022f0474809cc0ad58a8dbb620982
nagle <geektcp@163.com>
2019-09-21 23:02:47 +0800

    design pattern: facade

---
+++ alpha-design/alpha-design-pattern/src/test/java/com/geektcp/alpha/design/pattern/facade/FacadeTest.java
@@ -0,0 +1,11 @@
+package com.geektcp.alpha.design.pattern.facade;
+
+/**
+ * Created by TangHaiyang on 2019/9/21.
+ */
+public class FacadeTest {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.watchMovie();
+    }
+}

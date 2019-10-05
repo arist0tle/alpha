3e7e8fdfd1e8fb99cdf9233e16d28c0e6c8e913e
nagle <geektcp@163.com>
2019-10-05 21:03:32 +0800

    dubbo comsumer

---
+++ alpha-dubbo/alpha-dubbo-consumer/src/main/java/com/geektcp/alpha/dubbo/consumer/Consumer.java
@@ -0,0 +1,22 @@
+package com.geektcp.alpha.dubbo.consumer;
+
+import com.geektcp.alpha.dubbo.api.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
+        context.start();
+        DemoService demoService = (DemoService) context.getBean("demoService"); // get remote service proxy
+
+        try {
+            Thread.sleep(1000);
+            String hello = demoService.sayHello("Dubbo"); // call remote method
+            System.out.println(hello); // get result
+        } catch (Exception throwable) {
+            throwable.printStackTrace();
+        }
+
+    }
+}

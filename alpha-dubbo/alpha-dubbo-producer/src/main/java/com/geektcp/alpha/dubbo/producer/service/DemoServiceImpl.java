37b7e664f382f43a902dfa559c3860c0bf1e1409
nagle <geektcp@163.com>
2019-10-05 22:50:50 +0800

    dubbo demo source code config properties

--- alpha-dubbo/alpha-dubbo-producer/src/main/java/com/geektcp/alpha/dubbo/producer/service/DemoServiceImpl.java
+++ alpha-dubbo/alpha-dubbo-producer/src/main/java/com/geektcp/alpha/dubbo/producer/service/DemoServiceImpl.java
@@ -4,14 +4,14 @@ package com.geektcp.alpha.dubbo.producer.service;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.geektcp.alpha.dubbo.api.DemoService;
 
-import java.time.LocalDate;
+import java.time.Instant;
 
 @Service
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String sayHello(String name) {
-        System.out.println("[" + LocalDate.now() + "] Hello " + name  + "by annotation");
+        System.out.println("[" + Instant.now() + "] Hello " + name  + "by annotation");
         return "Hello " + name + " by annotation";
     }
 

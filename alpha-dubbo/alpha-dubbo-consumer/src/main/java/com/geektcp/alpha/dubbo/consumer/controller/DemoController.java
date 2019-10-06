e6d8f95400d84549048864c81bc2f20336d0431c
nagle <geektcp@163.com>
2019-10-06 22:10:30 +0800

    soa dubbo

--- alpha-dubbo/alpha-dubbo-consumer/src/main/java/com/geektcp/alpha/dubbo/consumer/controller/DemoController.java
+++ alpha-dubbo/alpha-dubbo-consumer/src/main/java/com/geektcp/alpha/dubbo/consumer/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.geektcp.alpha.dubbo.consumer.controller;
 
+import alpha.common.base.log.LogFactory;
+import alpha.common.base.log.TLog;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.geektcp.alpha.dubbo.api.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    private static TLog log = LogFactory.getLogger(DemoController.class);
 
     @Reference
     private DemoService demoService;
 
     @GetMapping("sayHello")
     public String sayHello(){
+        log.info("this controller: sayHello");
         return demoService.sayHello("Dubbo");
     }
 }
--- alpha-dubbo/alpha-dubbo-provider/src/main/java/com/geektcp/alpha/dubbo/provider/service/DemoServiceImpl.java
+++ alpha-dubbo/alpha-dubbo-provider/src/main/java/com/geektcp/alpha/dubbo/provider/service/DemoServiceImpl.java
@@ -10,11 +10,12 @@ import java.time.Instant;
 
 @Service
 public class DemoServiceImpl implements DemoService {
-//    private static TLog log = LogFactory.getLogger(DemoServiceImpl.class);
+    private static TLog log = LogFactory.getLogger(DemoServiceImpl.class);
 
     @Override
     public String sayHello(String name) {
-//        log.info("[" + Instant.now() + "] Hello " + name  + "by annotation");
+        log.info("this is service sayHello");
+        log.info("[" + Instant.now() + "] Hello " + name  + "by annotation");
         return "Hello " + name + " by annotation";
     }
 

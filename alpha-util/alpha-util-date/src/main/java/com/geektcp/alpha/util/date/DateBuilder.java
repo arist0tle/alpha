2fb278a3afc6bebf19145b5b2a884032b01c093e
geektcp <geektcp@163.com>
2019-10-24 11:30:02 +0800

    时间工具类

---
+++ alpha-util/alpha-util-date/src/main/java/com/geektcp/alpha/util/date/DateBuilder.java
@@ -0,0 +1,77 @@
+package com.geektcp.alpha.util.date;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * @author haiyang.tang on 10.24 024 10:08:31.
+ */
+public class DateBuilder {
+    private static final String pattern_cst = "yyyy-MM-dd HH:mm:ss";
+    private static final String pattern_std = "yyyy-MM-dd'T'HH:mm:ss";
+    private static final String pattern_gmt = "EEE, d MMM yyyy HH:mm:ss 'GMT'";
+    private static ThreadLocal<DateFormat> localDateFormat = new ThreadLocal<>();
+    private static ThreadLocal<DateFormat> localDateFormatDefault = new ThreadLocal<>();
+
+    public static DateFormat getDateFormat() {
+        DateFormat df = new SimpleDateFormat(pattern_cst);
+        localDateFormat.set(df);
+        return df;
+    }
+
+    private static DateFormat getDateFormat(String pattern, Locale locale) {
+        DateFormat df = new SimpleDateFormat(pattern, locale);
+        localDateFormat.set(df);
+        return df;
+    }
+
+
+    public static Date transform(String strDate) throws ParseException {
+        return getDateFormat().parse(strDate.replaceAll("T"," "));
+    }
+
+    public static Date transform(String parsePattern, String strDate) throws ParseException {
+        Locale locale = Locale.US;
+        if (strDate.contains("GMT")) {
+            locale = Locale.ENGLISH;
+        }
+        return getDateFormat(parsePattern, locale).parse(strDate);
+    }
+
+    public static String formatDefaultDate(Date date) {
+        return getDateFormat().format(date);
+    }
+
+    public static String formatDefaultDate(String date) {
+        return getDateFormat().format(date);
+    }
+
+    public static String format(String parsePattern, String strDate) throws ParseException {
+        Date date = transform(parsePattern,strDate);
+        return getDateFormat().format(date);
+    }
+
+    public static String format(String strDate) throws ParseException {
+        Date date = transform(strDate);
+        return getDateFormat().format(date);
+    }
+
+
+    /**
+     * @author haiyang.tang on 10.24 024 10:58:40.
+     */
+    public enum TimeZone {
+        CST,
+        GMT,
+        CET,
+        UTC,
+        IST,
+        EST,
+        MST,
+        PST,
+        ;
+    }
+}

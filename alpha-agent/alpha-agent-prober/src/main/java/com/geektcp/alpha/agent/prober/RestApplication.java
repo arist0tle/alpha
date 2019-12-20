86b2f44710b768e4a5fc5ec763f25a534acb927d
geektcp <geektcp@163.com>
2019-12-21 01:30:37 +0800

    probe

--- alpha-agent/alpha-agent-prober/src/main/java/com/geektcp/alpha/agent/prober/RestApplication.java
+++ alpha-agent/alpha-agent-prober/src/main/java/com/geektcp/alpha/agent/prober/RestApplication.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,8 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
     public static void main(String[] args) {
-        // TODO: Currently need to create Tracer directly under need 'tracer resolver' mechanism is available
-        APMTracer tracer=new APMTracer();
+        APMTracer tracer = new APMTracer();
         GlobalTracer.register(tracer);
         SpringApplication.run(RestApplication.class, args);
     }

8522667e6d7436fa806a85587756e04e5e34af54
geektcp <geektcp@163.com>
2020-01-02 01:20:43 +0800

    grpc case

---
+++ alpha-socket/alpha-grpc/src/main/java/com/geektcp/alpha/socket/grpc/SpringGrpcApplication.java
@@ -0,0 +1,15 @@
+package com.geektcp.alpha.socket.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tanghaiyang on 2020/1/2 1:18.
+ */
+@SpringBootApplication
+public class SpringGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGrpcApplication.class, args);
+    }
+}

392f7ab118acd45d02770d07f40b5ff19dd26f2a
geektcp <geektcp@163.com>
2020-02-04 19:21:45 +0800

    规范路径 alpha-console-modules all

---
+++ alpha-console/alpha-console-modules/alpha-console-modules-sso/src/main/java/com/geektcp/alpha/console/modules/sso/VoleSsoApplication.java
@@ -0,0 +1,31 @@
+package com.geektcp.alpha.console.modules.sso;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.geektcp.alpha.console.modules.sso", "com.geektcp.alpha.console.common.core.bean"})
+public class VoleSsoApplication {
+
+    public static void main(String[] args) {
+        log.debug("VoleSsoclientApplication startup main");
+        SpringApplication application = new SpringApplication(VoleSsoApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
+
+
+    @LoadBalanced
+    @Bean
+    public OAuth2RestTemplate restTemplate(UserInfoRestTemplateFactory factory) {
+        return factory.getUserInfoRestTemplate();
+    }
+}

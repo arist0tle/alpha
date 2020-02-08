e08c1c49c20efcd4ecf02173ce55e3fb5a41c5e1
geektcp <geektcp@163.com>
2020-02-08 19:41:54 +0800

    spring security module

--- alpha-spring/alpha-spring-security/src/main/java/com/geektcp/alpha/spring/security/valueobject/UserView.java
+++ alpha-spring/alpha-spring-security/src/main/java/com/geektcp/alpha/spring/security/valueobject/UserView.java
@@ -1,38 +1,18 @@
 package com.geektcp.alpha.spring.security.valueobject;
 
+import lombok.Data;
+
 import java.util.List;
 
 /**
  * Created by Shuangyao
  * 23:10 2018/9/6
  */
+@Data
 public class UserView {
 
     private String userName;
     private String userDesc;
     private List<String> roleCodes;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserDesc() {
-        return userDesc;
-    }
-
-    public void setUserDesc(String userDesc) {
-        this.userDesc = userDesc;
-    }
-
-    public List<String> getRoleCodes() {
-        return roleCodes;
-    }
-
-    public void setRoleCodes(List<String> roleCodes) {
-        this.roleCodes = roleCodes;
-    }
 }

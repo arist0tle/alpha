682e38e974b1e888a49e99a07037660e2b7270d4
haiyang.tang <haiyang.tang@xxx.com>
2020-02-13 16:24:55 +0800

    review

--- alpha-driver/alpha-driver-querydsl/src/main/java/com/geektcp/alpha/spring/querydsl/service/config/SwaggerConfig.java
+++ alpha-driver/alpha-driver-querydsl/src/main/java/com/geektcp/alpha/spring/querydsl/service/config/SwaggerConfig.java
@@ -17,10 +17,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    public static final String SWAGGER_SCAN_BASE_PACKAGE = "com.geektcp.alpha.spring.querydsl.service.controller";
-    public static final String VERSION = "1.0.0";
 
-    ApiInfo apiInfo() {
+    private static final String SWAGGER_SCAN_BASE_PACKAGE = "com.geektcp.alpha.spring.querydsl.service.controller";
+    private static final String VERSION = "1.0.0";
+
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Swagger API")
                 .description("This is to show api description")

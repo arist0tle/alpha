39c98b9e32a234b73725584155030b361f36af36
haiyang <geektcp@163.com>
2020-04-08 14:26:52 +0800

    Merge remote-tracking branch 'upstream/master'
    Conflicts:
    	alpha-scheduler/alpha-scheduler-api/src/main/java/com/geektcp/alpha/scheduler/api/controller/JobCodeController.java

--- alpha-scheduler/alpha-scheduler-api/src/main/java/com/geektcp/alpha/scheduler/api/controller/JobApiController.java
+++ alpha-scheduler/alpha-scheduler-api/src/main/java/com/geektcp/alpha/scheduler/api/controller/JobApiController.java
@@ -4,19 +4,11 @@ import com.geektcp.alpha.scheduler.api.controller.annotation.PermissionLimit;
 import com.geektcp.alpha.scheduler.api.core.conf.XxlJobAdminConfig;
 import com.geektcp.alpha.scheduler.api.core.exception.XxlJobException;
 import com.geektcp.alpha.scheduler.api.core.util.JacksonUtil;
-<<<<<<< HEAD:alpha-scheduler/alpha-scheduler-api/src/main/java/com/geektcp/alpha/scheduler/api/controller/JobApiController.java
 import com.geektcp.alpha.scheduler.core.biz.AdminBiz;
 import com.geektcp.alpha.scheduler.core.biz.model.HandleCallbackParam;
 import com.geektcp.alpha.scheduler.core.biz.model.RegistryParam;
 import com.geektcp.alpha.scheduler.core.biz.model.ReturnT;
 import com.geektcp.alpha.scheduler.core.util.XxlJobRemotingUtil;
-=======
-import com.xxl.job.core.biz.AdminBiz;
-import com.xxl.job.core.biz.model.HandleCallbackParam;
-import com.xxl.job.core.biz.model.RegistryParam;
-import com.xxl.job.core.biz.model.ReturnT;
-import com.xxl.job.core.util.XxlJobRemotingUtil;
->>>>>>> upstream/master:alpha-scheduler/alpha-scheduler-api/src/main/java/com/geektcp/alpha/scheduler/api/controller/JobApiController.java
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

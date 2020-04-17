b54b5b5ad99db937e07b449a05554d76a0a5396c
haiyang <geektcp@163.com>
2020-04-17 14:10:03 +0800

    code review

--- alpha-spring/alpha-spring-security/src/main/java/com/geektcp/alpha/spring/security/auth/provider/TokenProvider.java
+++ alpha-spring/alpha-spring-security/src/main/java/com/geektcp/alpha/spring/security/auth/provider/TokenProvider.java
@@ -31,39 +31,29 @@ public class TokenProvider {
     private LoginParameters loginParameters;
 
     @Autowired
-    public void setAutowired(SecurityProperties properties,LoginParameters loginParameters) {
+    public void setAutowired(SecurityProperties properties, LoginParameters loginParameters) {
         this.loginParameters = loginParameters;
         this.properties = properties;
     }
-
-
-
 
     public String createToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(","));
-
-        long now = (new Date()).getTime();
-        if (Objects.isNull(properties.getTokenValidityInSeconds())) {
-            properties.setTokenValidityInSeconds(1000L);
-        }
-        Date validity = new Date(now + loginParameters.getExpiration());
         Date expireTime = new Date(System.currentTimeMillis() + loginParameters.getExpiration());
         String username = authentication.getName();
-        log.info("=============username: {}", username);
-        String token =  Jwts.builder()
-                .setSubject(authentication.getName())
+        String token = Jwts.builder()
+                .setSubject(username)
                 .setExpiration(expireTime)
                 .claim(AUTHORITIES_KEY, authorities)
                 .signWith(EncryptUtils.buildKey(loginParameters.getEncryptSecret()), SignatureAlgorithm.HS512)
-//                .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
         log.info("token: {}", token);
         return token;
     }
 
-    Authentication getAuthentication(String token) {
+    ////////////////////////////////////////////////////////////
+    private Authentication getAuthentication(String token) {
         Claims claims = Jwts.parser()
                 .setSigningKey(loginParameters.getEncryptSecret())
                 .parseClaimsJws(token)
@@ -79,23 +69,23 @@ public class TokenProvider {
         return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 
-    boolean validateToken(String authToken) {
+    private boolean validateToken(String authToken) {
         try {
             Jwts.parser().setSigningKey(loginParameters.getEncryptSecret()).parseClaimsJws(authToken);
             return true;
         } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT signature: {}",e.getMessage());
+            log.info("Invalid JWT signature: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-            log.info("Expired JWT token: {}",e.getMessage());
+            log.info("Expired JWT token: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT token: {}",e.getMessage());
+            log.info("Unsupported JWT token: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
-            log.info("JWT token compact of handler are invalid: {}",e.getMessage());
+            log.info("JWT token compact of handler are invalid: {}", e.getMessage());
         }
         return false;
     }
 
-    public String getToken(HttpServletRequest request) {
+    private String getToken(HttpServletRequest request) {
         final String requestHeader = request.getHeader(properties.getHeader());
         if (requestHeader != null && requestHeader.startsWith(properties.getTokenStartWith())) {
             return requestHeader.substring(7);
